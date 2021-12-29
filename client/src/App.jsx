dfbe70063a0422dd59b8143add2fac45c62a2822
Adriana Calvo-Matos <adriana.calvomatos@gmail.com>
2021-12-28 21:30:15 -0500

    create routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import GiftItem from './routes/GiftItem';
+import GiftUpdate from './routes/GiftUpdate';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='gifts/:id/' element={<GiftItem />} />
+      <Route path='gifts/:id/update' element={<GiftUpdate />} />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ client/src/routes/GiftItem.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GitftItem = () => {
+  return <h1>GiftItem</h1>;
+};
+
+export default GitftItem;
